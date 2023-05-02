43a52b38d3b82537437e4e69f51b8f26eb8e6d2c
slivcaigor <slivcaigor@gmail.com>
2023-05-02 17:40:07 +0200

    Other pages

---
+++ la-mia-pizzeria-static/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
