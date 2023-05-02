777fb094b490650d74bfb676e9bd70531c6c1234
slivcaigor <slivcaigor@gmail.com>
2023-05-02 15:45:54 +0200

    Esercizi Razor

---
+++ esercizi-razor/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using esercizi_razor.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace esercizi_razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ esercizi-razor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace esercizi_razor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
