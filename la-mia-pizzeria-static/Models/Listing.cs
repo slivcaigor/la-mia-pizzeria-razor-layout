be495abd0a602af2031280815f5bc2d715f19e36
slivcaigor <slivcaigor@gmail.com>
2023-05-02 16:51:52 +0200

    Views

---
+++ la-mia-pizzeria-static/Models/Listing.cs
@@ -0,0 +1,11 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Listing
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public bool IsNew { get; set; }
+    }
+}
